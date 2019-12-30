c3cc491d5137ada214d1339f888261da2e4ea174
iammiguelmx <miguelangelcamachocampos@gmail.com>
2019-12-29 20:06:01 -0600

    Commit :tada:

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:https://iammiguelmx/consume-api master:gh-pages
+git push -f git@github.com:iammiguelmx/consume-api master:gh-pages
 
 cd -
